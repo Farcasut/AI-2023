c44b950347e41314821489d07f39899e3ef6c05d
Farcasut <farcasialexandrububu@gmail.com>
2023-11-02 22:12:25 +0200

    Added time output + clean up

--- Homework_2/MostConstraint.h
+++ Homework_2/MostConstraint.h
@@ -8,9 +8,16 @@
 #include <algorithm>
 #include <iostream>
 
+using std::vector;
+using std::pair;
+using std::unordered_set;
+using std::map;
+using std::cout;
+using std::make_pair;
+
 class MostConstraint {
-    std::vector<std::pair<std::pair<int, int>, std::unordered_set<int>*>> values_domains;
-    std::map<std::pair<int, int>, std::unordered_set<int>> look_table;
+    vector<pair<pair<int, int>, unordered_set<int>*>> values_domains;
+    map<pair<int, int>, unordered_set<int>> look_table;
 
 public:
 
@@ -22,12 +29,12 @@ public:
         return a;
     }
 
-    void push(std::pair<int, int> location, std::unordered_set<int> values) {
+    void push(pair<int, int> location, unordered_set<int> values) {
         this->look_table[location] = values;
-        values_domains.emplace_back(std::make_pair(location, &this->look_table[location]));
+        values_domains.emplace_back(make_pair(location, &this->look_table[location]));
     }
 
-    std::pair<int, int> get_next_location(const std::vector<std::vector<int8_t>>& board) {
+    pair<int, int> get_next_location(const vector<vector<int8_t>>& board) {
         for(auto const& val : values_domains) {
             if(board[val.first.first][val.first.second] == 0) {
                 return {val.first.first, val.first.second};
@@ -36,11 +43,11 @@ public:
         return {-1, -1};
     }
 
-    std::unordered_set<int>& get_least_values_domain() {
+    unordered_set<int>& get_least_values_domain() {
         return *values_domains[0].second;
     } 
 
-    std::pair<int, int> get_least_values_domain_coordonates() const {
+    pair<int, int> get_least_values_domain_coordonates() const {
         return values_domains[0].first;
     }
     
@@ -56,11 +63,11 @@ public:
         });
     }
 
-    std::unordered_set<int>& lookup_domain_values(std::pair<int, int> location) {
+    unordered_set<int>& lookup_domain_values(pair<int, int> location) {
         return this->look_table[location]; 
     } 
 
-    MostConstraint update_domain(std::pair<int, int> location, int value) {
+    MostConstraint update_domain(pair<int, int> location, int value) {
         MostConstraint new_object = this->copy(*this); 
 
         for(int j = 0; j < 9; j++) {
@@ -83,7 +90,7 @@ public:
         return new_object;
     }
 
-    bool check_empty_domains(const std::vector<std::vector<int8_t>>& board) {
+    bool check_empty_domains(const vector<vector<int8_t>>& board) {
         for (int i = 0; i < board.size(); i++) {
             for(int j = 0; j < board[i].size(); j++) {
                 if (board[i][j] == 0 && this->look_table[{i,j}].empty()) 
@@ -94,23 +101,23 @@ public:
     }
 
     void dump() const {
-        std::cout << "-----------------[ Values Domains ] -----------------\n";
+        cout << "-----------------[ Values Domains ] -----------------\n";
         for(int i = 0; i < values_domains.size(); i++) {
-            std::cout << values_domains[i].first.first << ',' << values_domains[i].first.second << ": ";
+            cout << values_domains[i].first.first << ',' << values_domains[i].first.second << ": ";
             for(auto const& val : *values_domains[i].second) {
-                std::cout << val << ' ';
+                cout << val << ' ';
             }
-            std::cout << '\n';
+            cout << '\n';
         }
-        std::cout << "-----------------[ END Domains ] -----------------\n";
-        std::cout << "-----------------[ Lookup Table ] -----------------\n";
+        cout << "-----------------[ END Domains ] -----------------\n";
+        cout << "-----------------[ Lookup Table ] -----------------\n";
         for(auto const& val : this->look_table) {
-            std::cout << val.first.first << ',' << val.first.second << ": ";
+            cout << val.first.first << ',' << val.first.second << ": ";
             for(auto const& x : val.second) {
-                std::cout << x << ' ';
+                cout << x << ' ';
             }
-            std::cout << '\n';
+            cout << '\n';
         }
-        std::cout << "-----------------[ END Lookup Table ] -----------------\n";
+        cout << "-----------------[ END Lookup Table ] -----------------\n";
     }
 };
